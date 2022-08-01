7293e005756869ed5a7ffe3886f252c8c2560ec7
Developer1 <Devloper1@gmail.com>
2022-08-01 13:56:32 +0530

    added main method in DemoGit class by dev1

--- DemoProj/src/com/nit/beans/DemoGit.java
+++ DemoProj/src/com/nit/beans/DemoGit.java
@@ -1,5 +1,12 @@
 package com.nit.beans;
 
+import com.nit.test.Test;
+
 public class DemoGit {
 
+	public static void main(String[] args) {
+		
+		Test t = new Test();
+		System.out.println(t.todayDate());
+	}
 }
