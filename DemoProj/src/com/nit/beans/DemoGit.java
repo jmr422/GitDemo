c1815f97f6259898cc65853ce92c6c3dc9f8bdfc
Developer1 <Devloper1@gmail.com>
2022-08-01 14:42:00 +0530

    Demo class main method changed by TL after deleting Test class

--- DemoProj/src/com/nit/beans/DemoGit.java
+++ DemoProj/src/com/nit/beans/DemoGit.java
@@ -1,12 +1,12 @@
 package com.nit.beans;
 
-import com.nit.test.Test;
+import java.util.Date;
 
 public class DemoGit {
 
 	public static void main(String[] args) {
 		
-		Test t = new Test();
-		System.out.println(t.todayDate());
+		
+		System.out.println(new Date());
 	}
 }
